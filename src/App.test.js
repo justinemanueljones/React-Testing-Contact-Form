de5e2b80bc13be89cb0ee93ce5f4553317d6dab8
justin-jones <52481185+justinemanueljones@users.noreply.github.com>
2020-10-29 13:35:39 -0400

    init commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import * as rtl from '@testing-library/react'
+import 'jest-dom/extend-expect'
+import App from './App'
 
-test("renders App without crashing", () => {
-  render(<App />);
+//install @testing-libary/react react-test-render jest-dom --save-dev
+//describe our test and wrap around our App
+
+it('returns a label element w/ First Name ', () => {
+  const wrapper = rtl.render(
+    <ContactForm/>
+  );
+  console.log(wrapper.debug())
 });
